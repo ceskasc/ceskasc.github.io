181500bc4d33727db663dabe224d205984d1e062
Sadık Can Çeşka <152553355+ceskasc@users.noreply.github.com>
2026-08-19 01:09:48 +0300

    Refresh O. V2 offline cache

--- o/sw.js
+++ o/sw.js
@@ -1,4 +1,4 @@
-const CACHE='o-circle-v2-3';
+const CACHE='o-circle-v2-4';
 const ASSETS=['./','./index.html','./styles.css','./config.js','./app.js','./app-core.js','./app-engine.js','./app-ui.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
