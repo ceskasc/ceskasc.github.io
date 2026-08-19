4ae82055ed7c9b752f36a03db91d41f336ac6b9d
Sadık Can Çeşka <152553355+ceskasc@users.noreply.github.com>
2026-08-19 10:00:32 +0300

    O. v9 rotate PWA cache

--- o/sw.js
+++ o/sw.js
@@ -1,5 +1,5 @@
-const CACHE='o-circle-v8-1';
-const ASSETS=['./','./index.html','./styles.css?v=8.0','./retention.css?v=8.1','./progress.css?v=8.1','./rivals.css?v=8.1','./v7.css?v=8.1','./v8.css?v=8.1','./app.js?v=8.0','./app-core.js?v=8.1','./app-engine.js?v=8.1','./app-ui.js?v=8.1','./progress.js?v=8.1','./rivals.js?v=8.1','./v7.js?v=8.1','./v8.js?v=8.1','./config.js?v=8.0','./manifest.webmanifest','./icon.svg'];
+const CACHE='o-circle-v9-0';
+const ASSETS=['./','./index.html','./styles.css?v=9.0','./retention.css?v=9.0','./progress.css?v=9.0','./rivals.css?v=9.0','./v7.css?v=9.0','./v8.css?v=9.0','./v9.css?v=9.0','./app.js?v=9.0','./app-core.js?v=9.0','./app-engine.js?v=9.0','./app-ui.js?v=9.0','./progress.js?v=9.0','./rivals.js?v=9.0','./v7.js?v=9.0','./v8.js?v=9.0','./v9.js?v=9.0','./config.js?v=9.0','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
